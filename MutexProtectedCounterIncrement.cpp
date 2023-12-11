194ff03c53f376135f509eeb3f52c7e506e8c0bc
Fatih KARABAS <fatih.karabas@amadeus.com>
2023-12-11 11:40:49 +0300

    style change file of the ex.

--- MutexProtectedCounterIncrement.cpp
+++ MutexProtectedCounterIncrement.cpp
@@ -1,7 +1,33 @@
+
 #include <iostream>
 #include <thread>
 #include <mutex>
 
+
+//Bad Mutex-Protected Counter Increment
+int bad_counter = 0;
+
+void badRaceCondition(){
+
+    for (int i = 0; i<100000; ++i){
+        bad_counter++; //Race condition on shared variable.
+    }
+}
+
+int main(){
+
+    std::thread t1 (badRaceCondition);
+    std::thread t2 (badRaceCondition);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Counter: " << bad_counter << std::endl;
+
+    return 0;
+}
+
+//Bad Mutex-Protected Counter Increment
 int good_counter = 0;
 std::mutex counter_mutex;
 
